7ec66551987a2f7f22f7c1cb9383cbf242365398
Leon Hong <leonshong@gmail.com>
2009-12-24 19:21:30 -0800

    added logcat, 'install app' now attempts to delete app on emulator first before installing

--- Support/onebit/installapp.py
+++ Support/onebit/installapp.py
@@ -1,9 +1,32 @@
 import onebit
 import os
+import re
 
 template = onebit.env.get_template("installapp.html")
 
-print template.render(
-    title='Install Android App', 
-)
+try:
+    f = open(os.environ['TM_PROJECT_DIRECTORY']+"/AndroidManifest.xml")
+    text = f.read()
+    com = re.compile('package[ ]*=[ ]*"[ 0-9a-zA-Z\.]+[ ]*"')
+    package=''
+    if com.search(text):
+        package = com.search(text).group(0).replace(" ","").replace('package=', '').replace('"','');
+
+
+    print template.render(
+        title='Install Android App', 
+        package = package,
+    )
+except Exception, e:
+    template = onebit.env.get_template("error.html")
+    print template.render(
+        title='Error',
+        error=os.environ['TM_PROJECT_DIRECTORY']+"/AndroidManifest.xml not found."
+    )
+
+
+
+
+
+
 
--- Support/onebit/logcat.py
+++ Support/onebit/logcat.py
@@ -1,8 +1,21 @@
 import onebit
 import os
+import commands
+import re
 
 template = onebit.env.get_template("logcat.html")
 
+cmd = "%s/tools/adb devices" % (os.environ['ANDROID_SDK'])
+devices = commands.getoutput(cmd)
+devices = devices.replace("List of devices attached \n", "").split("\n")
+device_str = ''
+device_count = 0
+for device in devices:
+    if device!='':
+        device_count+=1
+        device_str += (device.split("\t")[0]+" ")
+
 print template.render(
     title='Logcat', 
+    device_str=device_str,
 )
